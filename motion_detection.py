d2a63b57f810e82d8d5da569b51255235067e21d
Baas <matthew.baas@gmail.com>
2019-01-19 15:55:27 +0200

    Fix1

--- motion_detection.py
+++ motion_detection.py
@@ -1,28 +1,66 @@
 import numpy as np
 import picamera
 import picamera.array
+import datetime
+import time
 
-class Recorder(object):
+# class Recorder(object):
+#     def __init__(self, camera):
+#         self.busy = False
+#         self.camera = camera
+
+#     def record(self):
+#         if self.busy:
+#             return
+#         self.busy = True
+#         print("Starting Recording")
+#         self.camera.resolution = (640, 480)
+#         self.camera.start_recording('my_video.h264', splitter_port=2)
+#         self.camera.wait_recording(10, splitter_port=2)
+#         self.camera.stop_recording(splitter_port=2)
+#         print("Finished Recording")
+#         self.busy = False
+
+class Recorder:
     def __init__(self, camera):
-        self.busy = False
         self.camera = camera
+        self.detected = False
+        self.working = False
+        self.i = 0
 
-    def record(self):
-        if self.busy:
-            return
-        self.busy = True
-        print("Starting Recording")
-        self.camera.resolution = (640, 480)
-        self.camera.start_recording('my_video.h264', splitter_port=2)
-        self.camera.wait_recording(10, splitter_port=2)
-        self.camera.stop_recording(splitter_port=2)
-        print("Finished Recording")
-        self.busy = False
+    def motion_detected(self):
+        if not self.working:
+            self.detected = True
+
+    def tick(self):
+        if self.detected:
+            print("Started working on capturing")
+            self.working = True
+            self.detected = False
+            self.i += 1
+
+            path = "captures/%s/" % datetime.datetime.now().isoformat()
+
+            os.makedirs(path)
+
+            self.camera.start_preview()
+
+            for x in range(1, 16):
+                filename = "detected-%02d.jpg" % x
+                self.camera.capture(path + filename, use_video_port=True)
+                print("Captured " + filename)
+
+            self.camera.stop_preview()
+
+            print("Finished capturing")
+
+            self.working = False
 
 class DetectMotion(picamera.array.PiMotionAnalysis):
     def __init__(self, camera, recorder):
         super().__init__(camera)
         self.recorder = recorder
+        self.first = True
 
     def analyse(self, a):
         a = np.sqrt(
@@ -31,9 +69,12 @@ class DetectMotion(picamera.array.PiMotionAnalysis):
             ).clip(0, 255).astype(np.uint8)
         # If there're more than 10 vectors with a magnitude greater
         # than 60, then say we've detected motion
-        if (a > 40).sum() > 2:
+        if (a > 40).sum() > 10:#2:
             print('Motion detected!')
-            self.recorder.record()
+            if self.first:
+                self.first = False
+                return
+            self.recorder.motion_detected()
 
 class PiMotion(object):
     def __init__(self, callback=None, debug=False):
@@ -43,10 +84,22 @@ class PiMotion(object):
     def start(self):
         with picamera.PiCamera() as camera:
             camera.resolution = (1280, 720)
+            camera.framerate = 10
             recorder = Recorder(camera)
 
-            motion = DetectMotion(camera, recorder)
+            time.sleep(2)
 
-            camera.start_recording('/dev/null', format='h264', motion_output=motion)
-            camera.wait_recording(20)
-            camera.stop_recording()
+            motion = DetectMotion(camera, recorder)
+            try:
+                print("STARTED RECCCC")
+                camera.start_recording('/dev/null', format='h264', motion_output=motion)
+
+                while True:
+                    recorder.tick()
+                    time.sleep(1)
+            finally:
+                camera.stop_recording()
+                print("ENDED RECC")
+
+            #camera.wait_recording(20)
+            #camera.stop_recording()
